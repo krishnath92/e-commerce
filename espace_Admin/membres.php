1cc2b726051f552e3e789e6f00c1e33cea6cc347
krishnath92 <krish92130@gmail.com>
2022-02-13 23:44:49 +0100

    search member algo

--- espace_Admin/membres.php
+++ espace_Admin/membres.php
@@ -18,28 +18,46 @@ if(!$_SESSION['mdp']){
     <link rel="stylesheet" href="style/stylesheet.css" >
     <link rel="icon" type="image/png" href="../img/favicon.png">
     <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../design/accueil.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     
 
     
     <title>Afficher les membres</title>
 </head>
 <body>
-    <?php 
-        logoAdmin();
-        //Mise en place du menu
-        //initMenuAdminMembres();
-        
-    ?>
+    <?php logoAdmin(); ?>
+    <h1 style='font-size:48px; text-align:center;'>Les membres</h1>
+        <form method='get' action=''>
+            <div class="search-container">
+                    <input type="text" placeholder="Search.." name="search">
+                    <button class="bi bi-search"  type="submit" name ="ok"></button>
+            </div><hr/>
+        </form>
     
     <!--Afficher tous les membres-->
     <?php
-    $res = $db->query('SELECT count(*) from membres');
-    $recupUsers = $db->query('SELECT * from membres');
-    $nbrClients = $res->fetchColumn();
+
+    
+    if (!isset($_GET['search'])){
+        //Afficher tous les articles
+        $res = $db->query('SELECT count(*) from membres');
+        $recupUsers = $db->query('SELECT * from membres');
+
+        $nbrClients = $res->fetchColumn();
+    }
+
+    if (isset($_GET['search'])){
+        $carac = $_GET['search'];
+        $res = $db->query("SELECT count(*) from membres where civilité = '$carac' or prenom = '$carac' or nom = '$carac' or email = '$carac' or adresse_livraison = '$carac'");
+        $nbrClients = $res->fetchColumn();
+        $recupUsers = $db->prepare('SELECT * from membres where civilité = ? or prenom = ? or nom = ? or email = ? or adresse_livraison = ?');
+        $recupUsers->execute(array($carac,$carac,$carac,$carac,$carac));
+        
+    }
 
     echo"<table class = 'table-membre' border = '3' cellpadding='4' cellspacing ='1'>
                 <tr> 
-            <h1 style='text-align:center;'> Les clients-membres </h1> 
             <div style='text-align:center; margin-bottom:5px;'>
                 <b>Il y a " .  $nbrClients . " clients inscrits.</b>
             </div>" ?>
